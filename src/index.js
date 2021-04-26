4c4e7bc46c8731aacb53e56ba659e674d3663e9e
Jan Lunge <janlunge1993@gmail.com>
2021-04-26 14:36:03 +0200

    better reconnect functions
    Co-authored-by: Stefan Mitrovic <mitrovicstefan@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,14 +3,119 @@
 const express = require('express')
 const { URL } = require('url')
 const contentDisposition = require('content-disposition')
-const createRenderer = require('./renderer')
-
+const puppeteer = require('puppeteer-core')
 const port = process.env.PORT || 3000
 
 const app = express()
 
 let renderer = null
 
+class Renderer {
+  constructor(browser) {
+    this.browser = browser
+  }
+
+  async createPage(url, options = {}) {
+    const { timeout, waitUntil, ...extraOptions } = options
+    const page = await this.browser.newPage()
+
+    await page.goto(url, {
+      timeout: Number(extraOptions.timeout || 30 * 1000),
+      waitUntil: String(extraOptions.waitUntil || 'networkidle0'),
+    })
+    return page
+  }
+
+  async render(url, options = {}) {
+    let page = null
+    try {
+      const { timeout, waitUntil } = options
+      page = await this.createPage(url, { timeout, waitUntil })
+      const html = await page.content()
+      return html
+    } finally {
+      if (page) {
+        await page.close()
+      }
+    }
+  }
+
+  async pdf(url, options = {}) {
+    let page = null
+    try {
+      const { timeout, waitUntil, ...extraOptions } = options
+      page = await this.createPage(url, { timeout, waitUntil })
+
+      const { scale, displayHeaderFooter, printBackground, landscape } = extraOptions
+      const buffer = await page.pdf({
+        ...extraOptions,
+        scale: Number(scale),
+        displayHeaderFooter: displayHeaderFooter === 'true',
+        printBackground: printBackground === 'true',
+        landscape: landscape === 'true',
+      })
+      return buffer
+    } finally {
+      if (page) {
+        await page.close()
+      }
+    }
+  }
+
+  async screenshot(url, type, options = {}) {
+    let page = null
+    try {
+      const { timeout, waitUntil, ...extraOptions } = options
+      page = await this.createPage(url, { timeout, waitUntil })
+
+      const {
+        width,
+        height,
+        isMobile,
+        deviceScaleFactor,
+        hasTouch,
+        isLandscape,
+        media,
+        JSenabled,
+      } = extraOptions
+      await page.setViewport({
+        width: Number(width || 800),
+        height: Number(height || 600),
+        isMobile: Boolean(isMobile || false),
+        deviceScaleFactor: Number(deviceScaleFactor || 1),
+        hasTouch: Boolean(hasTouch || false),
+        isLandscape: Boolean(isLandscape || false),
+      })
+
+      var mediaType = String(media || 'screen')
+      var jsEnabled = Boolean(JSenabled || true)
+      await page.emulateMedia(mediaType)
+      await page.setJavaScriptEnabled(jsEnabled)
+
+      //await page.addScriptTag({content: 'alert("foo")'})
+      //await page.addStyleTag({content: 'body {background-color:red}'})
+
+      const { fullPage, omitBackground, quality } = extraOptions
+
+      const buffer = await page.screenshot({
+        ...extraOptions,
+        type: type,
+        quality: Number(quality) || (type === undefined || type == 'png' ? 0 : 100),
+        fullPage: fullPage === 'true',
+        omitBackground: omitBackground === 'true',
+      })
+      return buffer
+    } finally {
+      if (page) {
+        await page.close()
+      }
+    }
+  }
+
+  async close() {
+    await this.browser.close()
+  }
+}
 // Configure.
 app.disable('x-powered-by')
 
@@ -87,16 +192,38 @@ app.use((err, req, res, next) => {
   res.status(500).send('Ooops! An unexpected error seems to have occurred.')
 })
 
-const updateBrowser = (createdRenderer)=> {
+function updateBrowser(createdRenderer) {
     renderer = createdRenderer
 }
 
+async function create () {
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: process.env.CHROME_BIN || null,
+    args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage'],
+  })
+console.log('this is index')
+  browser.on('disconnected', async () => {
+    console.log('browser crashed')
+    let renderer = await create()
+    updateBrowser(renderer)
+  });
+  setTimeout(async () =>{
+    console.log('initial browser crashed')
+    let renderer = await create()
+    updateBrowser(renderer)
+  }, 30000)
+  console.log(`Started Puppeteer with pid ${browser.process().pid}`);
+
+  
+  return new Renderer(browser)
+}
+
 // Create renderer and start server.
-createRenderer()
+create()
   .then(createdRenderer => {
     renderer = createdRenderer
     console.info('Initialized renderer.')
-
     app.listen(port, () => {
       console.info(`Listen port on ${port}.`)
     })
@@ -110,4 +237,4 @@ process.on('SIGINT', () => {
   process.exit(0)
 })
 
-module.exports = updateBrowser
+
